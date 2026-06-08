6bde000e3ede0df43bb3d24cb1645e8f6ca7ab55
novus318 <nizamudheen318@gmail.com>
2026-06-08 15:25:08 +0530

    fix: refine outputFileTracingIncludes in next.config.ts for specific Chromium binaries

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   turbopack: {},
   outputFileTracingIncludes: {
     '/invoices/[id]/pdf': [
-      './node_modules/@sparticuz/chromium/bin/**/*',
+      'node_modules/@sparticuz/chromium/bin/chromium.br',
+      'node_modules/@sparticuz/chromium/bin/swiftshader.tar.br',
+      'node_modules/@sparticuz/chromium/bin/fonts.tar.br',
+      'node_modules/@sparticuz/chromium/bin/al2023.tar.br',
     ],
   },
 };
